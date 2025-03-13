c16b1029827a614f28d96829890bb222aa821049
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2025-03-13 19:03:52 -0400

    new config change

--- src/OperatorTemplate.Operator/Program.cs
+++ src/OperatorTemplate.Operator/Program.cs
@@ -1,21 +1,39 @@
+using k8s;
 using KubeOps.Operator;
 using SqlServerOperator.Configuration;
 using SqlServerOperator.Controllers.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddServiceDefaults();
 
 builder.Services.AddSingleton<SqlServerImages>();
 builder.Services.AddSingleton<DefaultMssqlConfig>();
 builder.Services.AddSingleton<SqlServerEndpointService>();
+
+// Add the Kubernetes client
+Kubernetes kubernetesClient;
+
+if (builder.Environment.IsProduction())
+{
+    // Use in-cluster config when running in Kubernetes
+    var kubernetesConfig = KubernetesClientConfiguration.InClusterConfig();
+    kubernetesClient = new Kubernetes(kubernetesConfig);
+}
+else
+{
+    // Use default kubeconfig for local development
+    var kubernetesConfig = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+    kubernetesClient = new Kubernetes(kubernetesConfig);
+}
+
+builder.Services.AddSingleton(kubernetesClient);
+builder.Services.AddKubernetesOperator();
 
 // Explicitly add Kubernetes client config
 builder.Services.AddKubernetesOperator();
 
 var app = builder.Build();
 
-app.MapDefaultEndpoints();
 app.UseKubernetesOperator();
 
 await app.RunOperatorAsync(args);
