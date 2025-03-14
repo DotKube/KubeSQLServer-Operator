d6a67b2f0656ad191fa1547bf5a1895eb41cc384
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2025-03-14 13:08:19 -0400

    add switch ns section to deploy.sh

--- deploy/yaml/deploy.sh
+++ deploy/yaml/deploy.sh
@@ -7,6 +7,10 @@ RAW_URL="https://raw.githubusercontent.com/DotKube/KubeSQLServer-Operator/main/d
 echo "Creating namespace '$NAMESPACE'..."
 kubectl create namespace "$NAMESPACE"
 
+# Switch to the namespace
+echo "Switching to namespace '$NAMESPACE'..."
+kubectl config set-context --current --namespace="$NAMESPACE"
+
 # Apply the YAML file
 echo "Applying deploy.yaml from $RAW_URL..."
 kubectl apply -f "$RAW_URL"
