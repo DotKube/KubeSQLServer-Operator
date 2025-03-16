6b63c8f69d55dc16ace79babdf292f04ef4f947f
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2025-03-16 19:26:46 -0400

    get app host working

--- src/OperatorTemplate.AppHost/Program.cs
+++ src/OperatorTemplate.AppHost/Program.cs
@@ -2,4 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var k8Operator = builder.AddProject<Projects.OperatorTemplate_Operator>("k8Operator");
 
+var docsSite = builder.AddNpmApp("documentation", "../../docs-site", "start")
+    .WithEnvironment("BROWSER", "none");
+
 builder.Build().Run();
